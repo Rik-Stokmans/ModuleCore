051d8551f33c3653385e4ad66d3b644321afdcdd
Rik <71502476+Rik-Stokmans@users.noreply.github.com>
2025-01-12 23:47:12 +0100

    Delete LogicLayer/Modules directory

--- LogicLayer/Modules/NuNlScraperModule/Interfaces/INewsObjectService.cs
+++
@@ -1,8 +0,0 @@
-using LogicLayer.Modules.NuNlScraperModule.Models;
-
-namespace LogicLayer.Modules.NuNlScraperModule.Interfaces;
-
-public interface INewsObjectService
-{
-    public List<NuNlObject> GetNewsObjects(string category);
-}
