4459e881684d4cdec798e32a82e04a966ba16ed7
Rik-Stokmans <71502476+Rik-Stokmans@users.noreply.github.com>
2025-01-17 15:08:57 +0100

    added a WarmUp.cs

--- FunctionApi/Functions/WarmUp.cs
+++ FunctionApi/Functions/WarmUp.cs
@@ -4,13 +4,25 @@ using LogicLayer.Authentication.Interfaces;
 using LogicLayer.Modules.ChildFocusModule.Interfaces;
 using LogicLayer.Modules.LoggingModule.Interfaces;
 using LogicLayer.Modules.NewsScraperModule.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
 using MockDataLayer.Services;
 
-namespace FunctionApi
+namespace FunctionApi.Functions;
+public class WarmUp(ILogger<WarmUp> logger)
 {
-    public static class Startup
+    [Function("WarmUp")]
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
     {
-        public static void startup()
+        logger.LogInformation("Processing request for Login.");
+        try
+        {
+            var service = Core.GetService<IAuthenticationService>();
+            return new AcceptedResult();
+        }
+        catch
         {
             Core.Init(registry =>
             {
@@ -20,6 +32,8 @@ namespace FunctionApi
                 registry.Register<IChildFocus>(new ChildFocusMockService());
                 registry.Register<INewsObjectService>(new NewsObjectTransientService());
             });
+            
+            return new OkResult();
         }
     }
 }
--- FunctionApi/Program.cs
+++ FunctionApi/Program.cs
@@ -1,6 +1,13 @@
+using AzureDatabase.Services;
+using LogicLayer;
+using LogicLayer.Authentication.Interfaces;
+using LogicLayer.Modules.ChildFocusModule.Interfaces;
+using LogicLayer.Modules.LoggingModule.Interfaces;
+using LogicLayer.Modules.NewsScraperModule.Interfaces;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MockDataLayer.Services;
 
 namespace FunctionApi;
 
@@ -8,6 +15,15 @@ public static class Program
 {
     public static void Main(string[] args)
     {
+        Core.Init(registry =>
+        {
+            // Register your services here
+            registry.Register<ILogService>(new AzureLogService());
+            registry.Register<IAuthenticationService>(new AzureAuthenticationService());
+            registry.Register<IChildFocus>(new ChildFocusMockService());
+            registry.Register<INewsObjectService>(new NewsObjectTransientService());
+        });
+        
         var host = new HostBuilder()
             .ConfigureFunctionsWebApplication()
             .ConfigureServices(services =>
