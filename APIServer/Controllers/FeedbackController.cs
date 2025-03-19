cd0272f5bfabea40b9c96ca35a89ebb39c585e79
Rik-Stokmans <71502476+Rik-Stokmans@users.noreply.github.com>
2025-03-19 14:14:32 +0100

    added api endpoints for getting info of all screens, and getting info for only 1 screen id

--- APIServer/Controllers/FeedbackController.cs
+++ APIServer/Controllers/FeedbackController.cs
@@ -29,10 +29,27 @@ public class FeedbackController(Context context) : ControllerBase
     
     [Authorize]
     [HttpGet]
+    [Route("{amount:int:range(1,100)}")]
+    public async Task<ActionResult<List<FeedbackView>>> GetLogsAsync(int amount = 20)
+    {
+        var feedbackItems = await context.FeedbackConditions.OrderByDescending(log => log.Time).Take(amount).ToListAsync();
+        var feedbackItemViews = feedbackItems.Select(feedback => feedback.GetFeedbackView()).ToList();
+        
+        return Ok(feedbackItemViews);
+    }
+    
+    [Authorize]
+    [HttpGet]
     [Route("{screenId}/{amount:int:range(1,100)}")]
     public async Task<ActionResult<List<FeedbackView>>> GetLogsAsync(string screenId, int amount = 20)
     {
-        var feedbackItems = await context.FeedbackConditions.OrderByDescending(log => log.Time).Take(amount).ToListAsync();
+        //check if the database contains the screenId
+        if (!await context.ScreenLocations.AnyAsync(location => location.ScreenId == screenId))
+        {
+            return NotFound("screenId not found");
+        }
+        
+        var feedbackItems = await context.FeedbackConditions.Where(feedback => feedback.ScreenId == screenId).OrderByDescending(log => log.Time).Take(amount).ToListAsync();
         var feedbackItemViews = feedbackItems.Select(feedback => feedback.GetFeedbackView()).ToList();
         
         return Ok(feedbackItemViews);
@@ -49,13 +66,29 @@ public class FeedbackController(Context context) : ControllerBase
         }
         
         //if the screenId is "all" i want to collect all the condition submitted in the timeframe, otherwise i want only the conditions for the screenId
-        var feedbackItems = 
-            screenId == "all" ? //can append more conditions if needed
-                await context.FeedbackConditions.Where(log => log.Time > DateTime.Now.AddDays(-daysBack)).ToListAsync() : //all screens will be selected for the statistics
-                await context.FeedbackConditions.Where(log => log.Time > DateTime.Now.AddDays(-daysBack) && log.ScreenId == screenId).ToListAsync(); //only screens matching the screenId will be given
+        var feedbackItems = await context.FeedbackConditions.Where(log => log.Time > DateTime.Now.AddDays(-daysBack) && log.ScreenId == screenId).ToListAsync(); //only screens matching the screenId will be given
         
         var total = feedbackItems.Count;
+        //for each condition, get the percentage of the total
+        var percentages = feedbackItems.GroupBy(log => log.Condition)
+            .Select(group => new KeyValuePair<FeedbackConditions, double>(group.Key, group.Count() / (double) total))
+            .ToList();
         
+        //order by percentage
+        percentages = percentages.OrderByDescending(pair => pair.Value).ToList();
+        
+        return Ok(percentages);
+    }
+    
+    [Authorize]
+    [HttpGet]
+    [Route("percentages/{daysBack:int:range(1,365)}")]
+    public async Task<ActionResult<List<KeyValuePair<FeedbackConditions, double>>>> GetPercentages(int daysBack = 7)
+    {
+        //if the screenId is "all" i want to collect all the condition submitted in the timeframe, otherwise i want only the conditions for the screenId
+        var feedbackItems = await context.FeedbackConditions.Where(log => log.Time > DateTime.Now.AddDays(-daysBack)).ToListAsync(); //only screens matching the screenId will be given
+        
+        var total = feedbackItems.Count;
         //for each condition, get the percentage of the total
         var percentages = feedbackItems.GroupBy(log => log.Condition)
             .Select(group => new KeyValuePair<FeedbackConditions, double>(group.Key, group.Count() / (double) total))
@@ -79,13 +112,9 @@ public class FeedbackController(Context context) : ControllerBase
         }
         
         //if the screenId is "all" i want to collect all the condition submitted in the timeframe, otherwise i want only the conditions for the screenId
-        var feedbackItems = 
-            screenId == "all" ? //can append more conditions if needed
-                await context.FeedbackConditions.Where(log => log.Time > startDate && log.Time < endDate).ToListAsync() : //all screens will be selected for the statistics
-                await context.FeedbackConditions.Where(log => log.Time > startDate && log.Time < endDate && log.ScreenId == screenId).ToListAsync(); //only screens matching the screenId will be given
+        var feedbackItems = await context.FeedbackConditions.Where(log => log.Time > startDate && log.Time < endDate && log.ScreenId == screenId).ToListAsync(); //only screens matching the screenId will be given
         
         var total = feedbackItems.Count;
-        
         //for each condition, get the percentage of the total
         var percentages = feedbackItems.GroupBy(log => log.Condition)
             .Select(group => new KeyValuePair<FeedbackConditions, double>(group.Key, group.Count() / (double) total))
@@ -97,5 +126,24 @@ public class FeedbackController(Context context) : ControllerBase
         return Ok(percentages);
     }
     
+    [Authorize]
+    [HttpGet]
+    [Route("percentages/{startDate:datetime}/{endDate:datetime}")]
+    public async Task<ActionResult<List<KeyValuePair<FeedbackConditions, double>>>> GetPercentages(DateTime startDate, DateTime endDate)
+    {
+        //if the screenId is "all" i want to collect all the condition submitted in the timeframe, otherwise i want only the conditions for the screenId
+        var feedbackItems = await context.FeedbackConditions.Where(log => log.Time > startDate && log.Time < endDate).ToListAsync(); //only screens matching the screenId will be given
+        
+        var total = feedbackItems.Count;
+        //for each condition, get the percentage of the total
+        var percentages = feedbackItems.GroupBy(log => log.Condition)
+            .Select(group => new KeyValuePair<FeedbackConditions, double>(group.Key, group.Count() / (double) total))
+            .ToList();
+        
+        //order by percentage
+        percentages = percentages.OrderByDescending(pair => pair.Value).ToList();
+        
+        return Ok(percentages);
+    }
     
 }
