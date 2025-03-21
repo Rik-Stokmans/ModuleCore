7632ca886e26baf22d463a05723886ec577062c8
Rik-Stokmans <71502476+Rik-Stokmans@users.noreply.github.com>
2025-03-21 14:46:39 +0100

    changed cors policies

--- APIServer/Program.cs
+++ APIServer/Program.cs
@@ -18,18 +18,15 @@ public static class Program
         
         builder.Services.AddCors(options =>
         {
-            //for production use
             options.AddPolicy("AllowAll",
-                policy => policy.WithOrigins("https://feedbackappapi-bxhpcgggcffaa3gu.westeurope-01.azurewebsites.net") // Change to your frontend URL
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    .AllowCredentials()); // Required for cookies
-            options.AddPolicy("AllowAll",
-                policy => policy.WithOrigins("http://localhost:63342") // Change to your frontend URL
+                policy => policy.WithOrigins(
+                        "https://feedbackappapi-bxhpcgggcffaa3gu.westeurope-01.azurewebsites.net",
+                        "http://localhost:63342") // Add multiple origins here
                     .AllowAnyMethod()
                     .AllowAnyHeader()
                     .AllowCredentials()); // Required for cookies
         });
+
         
         builder.Services.AddDbContext<Context>(options =>
         {
